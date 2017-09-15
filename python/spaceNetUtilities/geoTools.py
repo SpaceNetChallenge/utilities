8f410411a8e490a80bfa243dbe7541f32e712d4a
dlindenbaum <dlindenbaum@iqt.org>
2017-09-14 20:43:15 -0400

    removed latlon2pixel and replaced it with geomGeo2geomPixel.  Uses shapely affine transformations

--- python/spaceNetUtilities/geoTools.py
+++ python/spaceNetUtilities/geoTools.py
@@ -129,40 +129,30 @@ def exporttogeojson(geojsonfilename, geo_df):
     return geojsonfilename
 
 
-def latlon2pixel(lat, lon, input_raster='', targetsr='', geom_transform=''):
-    # type: (object, object, object, object, object) -> object
+def geomGeo2geomPixel(geom, affineObject=[], input_raster='', gdal_geomTransform=[]):
+    # This function transforms a shapely geometry in geospatial coordinates into pixel coordinates
+    # geom must be shapely geometry
+    # affineObject = rasterio.open(input_raster).affine
+    # gdal_geomTransform = gdal.Open(input_raster).GetGeoTransform()
+    # input_raster is path to raster to gather georectifcation information
+    if not affineObject:
+        if input_raster != '':
+            affineObject = rio.open(input_raster).affine
+        else:
+            affineObject = af.Affine.from_gdal(gdal_geomTransform)
 
-    sourcesr = osr.SpatialReference()
-    sourcesr.ImportFromEPSG(4326)
+    affineObjectInv = ~affineObject
 
-    geom = ogr.Geometry(ogr.wkbPoint)
-    geom.AddPoint(lon, lat)
+    geomTransform = shapely.affinity.affine_transform(geom,
+                                      [affineObjectInv.a,
+                                       affineObjectInv.b,
+                                       affineObjectInv.d,
+                                       affineObjectInv.e,
+                                       affineObjectInv.xoff,
+                                       affineObjectInv.yoff]
+                                      )
 
-    if targetsr == '':
-        src_raster = gdal.Open(input_raster)
-        targetsr = osr.SpatialReference()
-        targetsr.ImportFromWkt(src_raster.GetProjectionRef())
-    coord_trans = osr.CoordinateTransformation(sourcesr, targetsr)
-    if geom_transform == '':
-        src_raster = gdal.Open(input_raster)
-        transform = src_raster.GetGeoTransform()
-    else:
-        transform = geom_transform
-
-    x_origin = transform[0]
-    # print(x_origin)
-    y_origin = transform[3]
-    # print(y_origin)
-    pixel_width = transform[1]
-    # print(pixel_width)
-    pixel_height = transform[5]
-    # print(pixel_height)
-    geom.Transform(coord_trans)
-    # print(geom.GetPoint())
-    x_pix = (geom.GetPoint()[0] - x_origin) / pixel_width
-    y_pix = (geom.GetPoint()[1] - y_origin) / pixel_height
-
-    return (x_pix, y_pix)
+    return geomTransform
 
 
 def returnBoundBox(xOff, yOff, pixDim, inputRaster, targetSR='', pixelSpace=False):
