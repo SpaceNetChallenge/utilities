1ab22a6c47621ebea88441426feff2633010a919
dlindenbaum <dlindenbaum@iqt.org>
2017-09-18 15:09:01 -0400

    labelTools/pascalVOCLabel.py updated psacelVOCLabels for completeness

--- python/spaceNetUtilities/labelTools/pascalVOCLabel.py
+++ python/spaceNetUtilities/labelTools/pascalVOCLabel.py
@@ -1,32 +1,13 @@
 import numpy as np
 import os
-from spaceNetUtilities import geoTools as gT
-import math
-import pickle
-import csv
-import glob
+from spaceNetUtilities.labelTools import coreLabelTools as clT
 from PIL import Image
-from xml.etree.ElementTree import Element, SubElement, Comment, tostring
+from xml.etree.ElementTree import Element, SubElement
 from xml.etree import ElementTree
 from xml.dom import minidom
-import subprocess
-import scipy.io
-from scipy.sparse import csr_matrix
-import json
-import re
-import shapely
-import fiona
 import geopandas as gpd
 import rasterio
-from scipy.ndimage import morphology
 from rasterio import features
-from shapely.geometry.polygon import Polygon
-from shapely.geometry.multipolygon import MultiPolygon
-from shapely.geometry.linestring import LineString
-from shapely.geometry.multilinestring import MultiLineString
-from shapely.geometry import shape, box
-from shapely import affinity
-from osgeo import gdal, osr, ogr, gdalnumeric
 
 def prettify(elem):
     """Return a pretty-printed XML string for the Element.
@@ -106,7 +87,7 @@ def geoJsonToPASCALVOC2012Label(xmlFileName, geoJson, rasterImageName, im_id='',
                            objectType='building',
                            objectTypeField=''):
 
-    imageDescriptionDict, objectDictList = geoJsontoDict(geoJson, rasterImageName, datasetName='SpaceNet_V2',
+    imageDescriptionDict, objectDictList = clT.geoJsontoDict(geoJson, rasterImageName, datasetName=dataset,
                   annotationStyle=annotationStyle,
                   bboxResize=bboxResize,
                   objectType=objectType,
@@ -118,9 +99,64 @@ def geoJsonToPASCALVOC2012Label(xmlFileName, geoJson, rasterImageName, im_id='',
 
     xmlFileName = writeToPascalVOCLabel(xmlFileName, imageDescriptionDict, objectDictList)
 
+    return xmlFileName
 
 
-def geoJsonToPASCALVOC2012_Deprecated(xmlFileName, geoJson, rasterImageName, im_id='',
+def geoJsonToPASCALVOC2012SegmentCls(geoJson, src_meta, bufferSizePix=2.5,
+                                  innerShapeValue=100,
+                                  borderValue=255
+                                  ):
+
+    #TODO Implement multi object class segmentation
+
+    bufferDist = bufferSizePix*src_meta['transform'].a
+
+    source_layer = gpd.read_file(geoJson)
+    outerShapes = ((geom,value) for geom, value in zip(source_layer.geometry.buffer(bufferDist), borderValue))
+    innerShapes = ((geom, value) for geom, value in zip(source_layer.geometry.buffer(-bufferDist), innerShapeValue))
+
+    outerShapesImage = features.rasterize(outerShapes,
+                               out_shape=(src_meta['width'], src_meta['height']),
+                               transform=src_meta['transform'])
+
+    innerShapesImage = features.rasterize(outerShapes,
+                                   out_shape=(src_meta['width'], src_meta['height']),
+                                   transform=src_meta['transform'])
+
+    totalImage = outerShapesImage + innerShapesImage
+    # set interior value to be innerValue
+    totalImage[totalImage==innerShapeValue+borderValue]=innerShapeValue
+
+    return totalImage
+
+
+def geoJsonToPASCALVOC2012SegmentObj(geoJson, src_meta, bufferSizePix=2.5,
+                                  innerShapeValue=100,
+                                  borderValue=255
+                                  ):
+    # TODO Implement multi object class segmentation
+
+    bufferDist = bufferSizePix * src_meta['transform'].a
+
+    source_layer = gpd.read_file(geoJson)
+    outerShapes = ((geom, value) for geom, value in zip(source_layer.geometry.buffer(bufferDist), borderValue))
+    innerShapes = ((geom, value) for value, geom in enumerate(source_layer.geometry.buffer(-bufferDist)))
+
+    outerShapesImage = features.rasterize(outerShapes,
+                                          out_shape=(src_meta['width'], src_meta['height']),
+                                          transform=src_meta['transform'])
+
+    innerShapesImage = features.rasterize(outerShapes,
+                                          out_shape=(src_meta['width'], src_meta['height']),
+                                          transform=src_meta['transform'])
+
+    totalImage = outerShapesImage + innerShapesImage
+    # set interior value to be innerValue
+    totalImage[totalImage > 255] = totalImage[totalImage>255]-borderValue
+
+    return totalImage
+
+def geoJsonToPASCALVOC2012(xmlFileName, geoJson, rasterImageName, im_id='',
                            dataset ='SpaceNet',
                            folder_name='spacenet',
                            annotationStyle = 'PASCAL VOC2012',
@@ -129,212 +165,77 @@ def geoJsonToPASCALVOC2012_Deprecated(xmlFileName, geoJson, rasterImageName, im_
                            convertTo8Bit=True,
                            outputPixType='Byte',
                            outputFormat='GTiff',
-                           bboxResize=1.0):
-
-    print("creating {}".format(xmlFileName))
-    buildingList = gT.convert_wgs84geojson_to_pixgeojson(geoJson, rasterImageName, image_id=[], pixelgeojson=[], only_polygons=True,
-                                       breakMultiPolygonGeo=True, pixPrecision=2)
-    #                        buildinglist.append({'ImageId': image_id,
-                                             #'BuildingId': building_id,
-                                             #'polyGeo': ogr.CreateGeometryFromWkt(geom.ExportToWkt()),
-                                             #'polyPix': ogr.CreateGeometryFromWkt('POLYGON EMPTY')
-                                             #})
+                           bboxResize=1.0,
+                           objectType='building',
+                           objectTypeField='',
+                           clsPNGName='',
+                           objPNGName='',
+                           outputRasterName=''):
 
 
 
+    geoJsonToPASCALVOC2012Label(xmlFileName, geoJson, rasterImageName, im_id='',
+                                dataset=dataset,
+                                folder_name=folder_name,
+                                annotationStyle=annotationStyle,
+                                segment=segment,
+                                bufferSizePix=bufferSizePix,
+                                convertTo8Bit=convertTo8Bit,
+                                outputPixType=outputPixType,
+                                outputFormat=outputFormat,
+                                bboxResize=bboxResize,
+                                objectType=objectType,
+                                objectTypeField=objectTypeField)
+
+    with rasterio.open(rasterImageName) as src:
+        src_meta = src.meta.copy()
+        src_profile = src.profile.copy()
 
 
-    srcRaster = gdal.Open(rasterImageName)
-    outputRaster = rasterImageName
+
+    #Write Segemeentation Images
+    if segment:
+        if clsPNGName=='':
+            clsPNGName = xmlFileName.replace('.xml', 'segcls.tif')
+
+        clsImageArray = geoJsonToPASCALVOC2012SegmentCls(geoJson, src_meta, bufferSizePix=2.5,
+                                                         innerShapeValue=100,
+                                                         borderValue=255
+                                                         )
+
+        clsImageArray = Image.fromarray(clsImageArray)
+        clsImageArray.save(clsPNGName)
+
+        if objPNGName=='':
+            objPNGName = xmlFileName.replace('.xml', 'segobj.tif')
+
+        objImageArray = geoJsonToPASCALVOC2012SegmentObj(geoJson, src_meta, bufferSizePix=2.5,
+                                                         innerShapeValue=100,
+                                                         borderValue=255
+                                                         )
+        objImageArray = Image.fromarray(objImageArray)
+        objImageArray.save(objPNGName)
+
     if convertTo8Bit:
-        cmd = ['gdal_translate', '-ot', outputPixType, '-of', outputFormat, '-co', '"PHOTOMETRIC=rgb"']
-        scaleList = []
-        for bandId in range(srcRaster.RasterCount):
-            bandId = bandId+1
-            band=srcRaster.GetRasterBand(bandId)
-            min = band.GetMinimum()
-            max = band.GetMaximum()
+        if outputRasterName=='':
+            if outputFormat == 'JPEG':
+                outputImageName = xmlFileName.replace('.xml', '_8bit.jpg')
+            else:
+                outputImageName = xmlFileName.replace('.xml', '_8bit.tif')
 
-            # if not exist minimum and maximum values
-            if min is None or max is None:
-                (min, max) = band.ComputeRasterMinMax(1)
-            cmd.append('-scale_{}'.format(bandId))
-            cmd.append('{}'.format(0))
-            cmd.append('{}'.format(max))
-            cmd.append('{}'.format(0))
-            cmd.append('{}'.format(255))
-
-        cmd.append(rasterImageName)
-
-        if outputFormat == 'JPEG':
-            outputRaster = xmlFileName.replace('.xml', '.jpg')
-        else:
-            outputRaster = xmlFileName.replace('.xml', '.tif')
-
-        outputRaster = outputRaster.replace('_img', '_8bit_img')
-        cmd.append(outputRaster)
-        print(cmd)
-        subprocess.call(cmd)
-
-
-    if segment:
-        segmented = 1  # 1=True, 0 = False
+            clT.convertGTiffTo8Bit(rasterImageName, outputImageName, outputFormat=outputFormat)
     else:
-        segmented = 0
+        outputImageName = rasterImageName
 
-    top = Element('annotation')
-    ## write header
-    childFolder = SubElement(top, 'folder')
-    childFolder.text = dataset
-    childFilename = SubElement(top, 'filename')
-    childFilename.text = rasterImageName
-
-    # write source block
-    childSource = SubElement(top, 'source')
-    SubElement(childSource, 'database').text = dataset
-    SubElement(childSource, 'annotation').text = annotationStyle
-
-    # write size block
-    childSize = SubElement(top, 'size')
-    SubElement(childSize, 'width').text = str(srcRaster.RasterXSize)
-    SubElement(childSize, 'height').text = str(srcRaster.RasterYSize)
-    SubElement(childSize, 'depth').text = str(srcRaster.RasterCount)
-
-    SubElement(top, 'segmented').text = str(segmented)
-
-    # start object segment
-    for building in buildingList:
-        objectType = 'building'
-        objectPose = 'Left'
-        objectTruncated = 0  # 1=True, 0 = False
-        objectDifficulty = 0  # 0 Easy - 3 Hard
-
-        env = building['polyPix'].GetEnvelope()
-        xmin=env[0]
-        ymin=env[2]
-        xmax=env[1]
-        ymax=env[3]
-
-        if bboxResize != 1.0:
-            xCenter = (xmin+xmax)/2
-            yCenter = (ymin+ymax)/2
-            bboxNewHalfHeight = ((ymax-ymin)/2)*bboxResize
-            bboxNewHalfWidth  = ((ymax - ymin) / 2)*bboxResize
-            xmin = xCenter - bboxNewHalfWidth
-            xmax = xCenter + bboxNewHalfWidth
-            ymin = yCenter - bboxNewHalfHeight
-            ymax = yCenter + bboxNewHalfHeight
-
-        # Get Envelope returns a tuple (minX, maxX, minY, maxY)
-
-
-        childObject = SubElement(top, 'object')
-        SubElement(childObject, 'name').text = objectType
-        SubElement(childObject, 'pose').text = objectPose
-        SubElement(childObject, 'truncated').text = str(objectTruncated)
-        SubElement(childObject, 'difficult').text = str(objectDifficulty)
-        # write bounding box
-        childBoundBox = SubElement(childObject, 'bndbox')
-        SubElement(childBoundBox, 'xmin').text = str(int(round(xmin)))
-        SubElement(childBoundBox, 'ymin').text = str(int(round(ymin)))
-        SubElement(childBoundBox, 'xmax').text = str(int(round(xmax)))
-        SubElement(childBoundBox, 'ymax').text = str(int(round(ymax)))
-
-    with open(xmlFileName, 'w') as f:
-        f.write(prettify(top))
-
-
-    print('creating segmentation')
-    if segment:
-        NoData_value = -9999
-
-        source_ds = ogr.Open(geoJson)
-        source_layer = source_ds.GetLayer()
-        srs = source_layer.GetSpatialRef()
-        memDriver = ogr.GetDriverByName('MEMORY')
-        outerBuffer=memDriver.CreateDataSource('outer')
-        outerBufferLayer = outerBuffer.CreateLayer("test", srs, geom_type=ogr.wkbPolygon)
-        innerBuffer = memDriver.CreateDataSource('inner')
-        innerBufferLayer = innerBuffer.CreateLayer("test2", srs, geom_type=ogr.wkbPolygon)
-
-        idField = ogr.FieldDefn("objid", ogr.OFTInteger)
-        innerBufferLayer.CreateField(idField)
-
-        featureDefn = innerBufferLayer.GetLayerDefn()
-        bufferDist = srcRaster.GetGeoTransform()[1]*bufferSizePix
-        for idx, feature in enumerate(source_layer):
-            ingeom = feature.GetGeometryRef()
-            geomBufferOut = ingeom.Buffer(bufferDist)
-            geomBufferIn  = ingeom.Buffer(-bufferDist)
-            print(geomBufferIn.ExportToWkt())
-            print(geomBufferIn.IsEmpty())
-            print(geomBufferIn.IsSimple())
-
-            if geomBufferIn.GetArea()>0.0:
-                outBufFeature = ogr.Feature(featureDefn)
-                outBufFeature.SetGeometry(geomBufferOut)
-
-                outerBufferLayer.CreateFeature(outBufFeature)
-
-                inBufFeature = ogr.Feature(featureDefn)
-                inBufFeature.SetGeometry(geomBufferIn)
-                inBufFeature.SetField('objid', idx)
-                innerBufferLayer.CreateFeature(inBufFeature)
-
-                outBufFeature = None
-                inBufFeature = None
-
-
-
-        print('writing GTIFF sgcls')
-        print('rasterToWrite = {}'.format(xmlFileName.replace('.xml', 'segcls.tif')))
-        target_ds = gdal.GetDriverByName('GTiff').Create(xmlFileName.replace('.xml', 'segcls.tif'), srcRaster.RasterXSize, srcRaster.RasterYSize, 1, gdal.GDT_Byte)
-        print('setTransform')
-        target_ds.SetGeoTransform(srcRaster.GetGeoTransform())
-        print('setProjection')
-        target_ds.SetProjection(srcRaster.GetProjection())
-        print('getBand')
-        band = target_ds.GetRasterBand(1)
-        print('setnodata')
-        band.SetNoDataValue(NoData_value)
-
-        # Rasterize
-        print('rasterize outer buffer')
-        gdal.RasterizeLayer(target_ds, [1], outerBufferLayer, burn_values=[255])
-        print('rasterize inner buffer')
-        gdal.RasterizeLayer(target_ds, [1], innerBufferLayer, burn_values=[100])
-        print('writing png sgcls')
-        # write to .png
-        imageArray = np.array(target_ds.GetRasterBand(1).ReadAsArray())
-        im = Image.fromarray(imageArray)
-        im.save(xmlFileName.replace('.xml', 'segcls.png'))
-
-        print('writing GTIFF sgobj')
-        ## create objectSegment
-        target_ds = gdal.GetDriverByName('GTiff').Create(xmlFileName.replace('.xml', 'segobj.tif'),
-                                                         srcRaster.RasterXSize, srcRaster.RasterYSize, 1, gdal.GDT_Byte)
-        target_ds.SetGeoTransform(srcRaster.GetGeoTransform())
-        target_ds.SetProjection(srcRaster.GetProjection())
-        band = target_ds.GetRasterBand(1)
-
-        band.SetNoDataValue(NoData_value)
-
-        # Rasterize
-        gdal.RasterizeLayer(target_ds, [1], outerBufferLayer, burn_values=[255])
-        gdal.RasterizeLayer(target_ds, [1], innerBufferLayer, burn_values=[100], options=['ATTRIBUTE=objid'])
-        print('writing png sgobj')
-        # write to .png
-        imageArray = np.array(target_ds.GetRasterBand(1).ReadAsArray())
-        im = Image.fromarray(imageArray)
-        im.save(xmlFileName.replace('.xml', 'segobj.png'))
-
-    entry = {'rasterFileName': outputRaster,
+    entry = {'rasterFileName': outputImageName,
                  'geoJsonFileName': geoJson,
                  'annotationName': xmlFileName,
-                 'width': srcRaster.RasterXSize,
-                 'height': srcRaster.RasterYSize,
-                 'depth' : srcRaster.RasterCount,
+                 'width': src_meta['width'],
+                 'height': src_meta['height'],
+                 'depth' : src_meta['count'],
+                 'src_meta': src_meta,
                  'basename': os.path.splitext(os.path.basename(rasterImageName))[0]
                  }
 
     return entry
+
