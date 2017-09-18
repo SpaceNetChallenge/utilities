65927d0ed2f1628d0b7702014d8fa851e5514353
dlindenbaum <dlindenbaum@iqt.org>
2017-09-18 15:16:58 -0400

    initial move of sbdLabel functionality to labelTools/sbdLabel.py and removal of it from labelTools/coreLabelTools.py

--- python/spaceNetUtilities/labelTools/coreLabelTools.py
+++ python/spaceNetUtilities/labelTools/coreLabelTools.py
@@ -110,61 +110,10 @@ def convertLabelStringToPoly(shapeFileSrc, outGeoJSon, labelType='Airplane', asp
 
 
 def createNPPixArrayDist(rasterSrc, vectorSrc, npDistFileName='', units='pixels'):
-    #TODO evaluate scipy.ndimage.morphology.distance_transform_edt vs cv2.distanceTransform for speed
 
-    # image = features.rasterize(
-    #         ((g, 255) for g, v in shapes),
-    #         out_shape=src.shape,
-    #         transform=src.transform)
-    ## open source vector file that truth data
-    ## calculate pixelSize in meters i.e. GSD
+    print('Deprecated, use createDistanceTransform')
 
-    with rasterio.open(rasterSrc) as srcRas_ds:
-        src_transform = srcRas_ds.transform
-        src_shape = srcRas_ds.shape
-        ## calculate pixel size in meters i.e. GSD
-        if units == 'meters':
-            geoTrans, poly, ulX, ulY, lrX, lrY = gT.getRasterExtent(srcRas_ds)
-            transform_WGS84_To_UTM, transform_UTM_To_WGS84, utm_cs = gT.createUTMTransform(poly)
-            line = LineString([(geoTrans.c, geoTrans.f),
-                               (geoTrans.c + geoTrans.a, geoTrans.f + geoTrans.e)
-                               ]
-                              )
-
-            line = shapely.ops.tranform(transform_WGS84_To_UTM, line)
-            metersIndex = line.lenth
-        else:
-            metersIndex = 1
-
-    ## Burn source layer into image
-    source_layer = gpd.read_file(vectorSrc)
-    shapes = ((geom,value) for geom, value in zip(source_layer.geometry, 255))
-    baseImage = features.rasterize(shapes,
-                               out_shape=src_shape,
-                               transform=src_transform)
-
-    ## calculate Distance between Feature point and closest background pixel
-    # distance_transform_edt takes the place of ComputeProximity
-
-    #calculate distance from Any point inside a feature to the closest background pixel
-    interiorDist = morphology.distance_transform_edt(baseImage)
-
-    #inverse Image so that background is a feature and interior feature values = 0
-    inverseImage = baseImage
-    inverseImage[inverseImage==0]=200
-    inverseImage[inverseImage==255]=0
-
-    # calculate distance from any point exterior a feature to the closet feature pixel
-    exteriorDist = morphology.distance_transform_edt(inverseImage)
-
-
-    proxTotal = interiorDist - exteriorDist
-    proxTotal = proxTotal*metersIndex
-
-    if npDistFileName != '':
-        np.save(npDistFileName, proxTotal)
-
-    return proxTotal
+    return createDistanceTransform(rasterSrc, vectorSrc, npDistFileName=npDistFileName, units=units)
 
 
 def polygonize(imageArray, transformAffineObject, maskValue=0):
@@ -716,67 +665,55 @@ def geoJsonToDARKNET(xmlFileName, geoJson, rasterImageName, im_id='',
 
 def createDistanceTransform(rasterSrc, vectorSrc, npDistFileName='', units='pixels'):
 
+    #TODO evaluate scipy.ndimage.morphology.distance_transform_edt vs cv2.distanceTransform for speed
+
+    # image = features.rasterize(
+    #         ((g, 255) for g, v in shapes),
+    #         out_shape=src.shape,
+    #         transform=src.transform)
     ## open source vector file that truth data
-    source_ds = ogr.Open(vectorSrc)
-    source_layer = source_ds.GetLayer()
+    ## calculate pixelSize in meters i.e. GSD
 
-    ## extract data from src Raster File to be emulated
-    ## open raster file that is to be emulated
-    srcRas_ds = gdal.Open(rasterSrc)
-    cols = srcRas_ds.RasterXSize
-    rows = srcRas_ds.RasterYSize
-    noDataValue = 0
+    with rasterio.open(rasterSrc) as srcRas_ds:
+        src_transform = srcRas_ds.transform
+        src_shape = srcRas_ds.shape
+        ## calculate pixel size in meters i.e. GSD
+        if units == 'meters':
+            geoTrans, poly, ulX, ulY, lrX, lrY = gT.getRasterExtent(srcRas_ds)
+            transform_WGS84_To_UTM, transform_UTM_To_WGS84, utm_cs = gT.createUTMTransform(poly)
+            line = LineString([(geoTrans.c, geoTrans.f),
+                               (geoTrans.c + geoTrans.a, geoTrans.f + geoTrans.e)
+                               ]
+                              )
 
-    if units=='meters':
-        geoTrans, poly, ulX, ulY, lrX, lrY = gT.getRasterExtent(srcRas_ds)
-        transform_WGS84_To_UTM, transform_UTM_To_WGS84, utm_cs = gT.createUTMTransform(poly)
-        line = ogr.Geometry(ogr.wkbLineString)
-        line.AddPoint(geoTrans.c, geoTrans.f)
-        line.AddPoint(geoTrans.c+geoTrans.a, geoTrans.f)
+            line = shapely.ops.tranform(transform_WGS84_To_UTM, line)
+            metersIndex = line.lenth
+        else:
+            metersIndex = 1
 
-        line.Transform(transform_WGS84_To_UTM)
-        metersIndex = line.Length()
-    else:
-        metersIndex = 1
+    ## Burn source layer into image
+    source_layer = gpd.read_file(vectorSrc)
+    shapes = ((geom,value) for geom, value in zip(source_layer.geometry, 255))
+    baseImage = features.rasterize(shapes,
+                               out_shape=src_shape,
+                               transform=src_transform)
 
-    ## create First raster memory layer
-    memdrv = gdal.GetDriverByName('MEM')
-    dst_ds = memdrv.Create('', cols, rows, 1, gdal.GDT_Byte)
-    dst_ds.SetGeoTransform(srcRas_ds.GetGeoTransform())
-    dst_ds.SetProjection(srcRas_ds.GetProjection())
-    band = dst_ds.GetRasterBand(1)
-    band.SetNoDataValue(noDataValue)
+    ## calculate Distance between Feature point and closest background pixel
+    # distance_transform_edt takes the place of ComputeProximity
 
-    gdal.RasterizeLayer(dst_ds, [1], source_layer, burn_values=[255])
-    srcBand = dst_ds.GetRasterBand(1)
+    #calculate distance from Any point inside a feature to the closest background pixel
+    interiorDist = morphology.distance_transform_edt(baseImage)
 
-    memdrv2 = gdal.GetDriverByName('MEM')
-    prox_ds = memdrv2.Create('', cols, rows, 1, gdal.GDT_Int16)
-    prox_ds.SetGeoTransform(srcRas_ds.GetGeoTransform())
-    prox_ds.SetProjection(srcRas_ds.GetProjection())
-    proxBand = prox_ds.GetRasterBand(1)
-    proxBand.SetNoDataValue(noDataValue)
-    options = ['NODATA=0']
-    
-    ##compute distance to non-zero pixel values and scrBand and store in proxBand
-    gdal.ComputeProximity(srcBand, proxBand, options)
+    #inverse Image so that background is a feature and interior feature values = 0
+    inverseImage = baseImage
+    inverseImage[inverseImage==0]=200
+    inverseImage[inverseImage==255]=0
 
-    memdrv3 = gdal.GetDriverByName('MEM')
-    proxIn_ds = memdrv3.Create('', cols, rows, 1, gdal.GDT_Int16)
-    proxIn_ds.SetGeoTransform(srcRas_ds.GetGeoTransform())
-    proxIn_ds.SetProjection(srcRas_ds.GetProjection())
-    proxInBand = proxIn_ds.GetRasterBand(1)
-    proxInBand.SetNoDataValue(noDataValue)
-    options = ['NODATA=0', 'VALUES=0']
-    
-    ##compute distance to zero pixel values and scrBand and store in proxInBand
-    gdal.ComputeProximity(srcBand, proxInBand, options)
+    # calculate distance from any point exterior a feature to the closet feature pixel
+    exteriorDist = morphology.distance_transform_edt(inverseImage)
 
-    proxIn = gdalnumeric.BandReadAsArray(proxInBand)
-    proxOut = gdalnumeric.BandReadAsArray(proxBand)
- 
-    ##distance tranform is the distance to zero pixel values minus distance to non-zero pixel values
-    proxTotal = proxIn.astype(float) - proxOut.astype(float)
+
+    proxTotal = interiorDist - exteriorDist
     proxTotal = proxTotal*metersIndex
 
     if npDistFileName != '':
@@ -785,202 +722,9 @@ def createDistanceTransform(rasterSrc, vectorSrc, npDistFileName='', units='pixe
     return proxTotal
 
 
-def createClassSegmentation(rasterSrc, vectorSrc, npDistFileName='', units='pixels'):
-    dist_trans = createDistanceTransform(rasterSrc, vectorSrc, npDistFileName='', units='pixels')
-    dist_trans[dist_trans > 0] = 1
-    dist_trans[dist_trans < 0] = 0
-    return dist_trans
-
-
-def createClassBoundaries(rasterSrc, vectorSrc, npDistFileName='', units='pixels'):
-    dist_trans = createDistanceTransform(rasterSrc, vectorSrc, npDistFileName='', units='pixels')
-    #From distance transform to boundary
-    dist_trans[dist_trans > 1.0] = 255
-    dist_trans[dist_trans < -1.0] = 255
-    dist_trans[dist_trans != 255] = 1
-    dist_trans[dist_trans == 255] = 0
-    sparse_total = csr_matrix(dist_trans)
-    return sparse_total.astype(np.uint8)
-
-
-def createClassCategoriesPresent(vectorSrc):
-    with open(vectorSrc) as my_file:
-        data = json.load(my_file)
-    if(len(data['features']) == 0):
-       return np.array([],dtype=np.uint8)
-    else:
-       return np.array([1],dtype=np.uint8)
-
-def createDistanceTransformByFeatureIndex(feature_index, rasterSrc, vectorSrc, npDistFileName='', units='pixels'):
-    ## open source vector file that truth data
-    source_ds = ogr.Open(vectorSrc)
-    source_layer = source_ds.GetLayer()
-    
-    #Define feature
-    my_feature = source_layer[feature_index]
-    
-    #Spatial Reference
-    srs = source_layer.GetSpatialRef()
-    
-    #Create feature Layer
-    outDriver = ogr.GetDriverByName('MEMORY')
-    outDataSource = outDriver.CreateDataSource('memData')
-    Feature_Layer = outDataSource.CreateLayer("this_feature", srs, geom_type=ogr.wkbPolygon)
-    
-    #Add feature to layer
-    Feature_Layer.CreateFeature(my_feature)
-    
-
-    ## extract data from src Raster File to be emulated
-    ## open raster file that is to be emulated
-    srcRas_ds = gdal.Open(rasterSrc)
-    cols = srcRas_ds.RasterXSize
-    rows = srcRas_ds.RasterYSize
-    noDataValue = 0
-    metersIndex = 1
-
-    ## create First raster memory layer
-    memdrv = gdal.GetDriverByName('MEM')
-    dst_ds = memdrv.Create('', cols, rows, 1, gdal.GDT_Byte)
-    dst_ds.SetGeoTransform(srcRas_ds.GetGeoTransform())
-    dst_ds.SetProjection(srcRas_ds.GetProjection())
-    band = dst_ds.GetRasterBand(1)
-    band.SetNoDataValue(noDataValue)
-
-    gdal.RasterizeLayer(dst_ds, [1], Feature_Layer, burn_values=[255])
-    srcBand = dst_ds.GetRasterBand(1)
-
-    memdrv2 = gdal.GetDriverByName('MEM')
-    prox_ds = memdrv2.Create('', cols, rows, 1, gdal.GDT_Int16)
-    prox_ds.SetGeoTransform(srcRas_ds.GetGeoTransform())
-    prox_ds.SetProjection(srcRas_ds.GetProjection())
-    proxBand = prox_ds.GetRasterBand(1)
-    proxBand.SetNoDataValue(noDataValue)
-
-    options = ['NODATA=0']
-
-    gdal.ComputeProximity(srcBand, proxBand, options)
-
-    memdrv3 = gdal.GetDriverByName('MEM')
-    proxIn_ds = memdrv3.Create('', cols, rows, 1, gdal.GDT_Int16)
-    proxIn_ds.SetGeoTransform(srcRas_ds.GetGeoTransform())
-    proxIn_ds.SetProjection(srcRas_ds.GetProjection())
-    proxInBand = proxIn_ds.GetRasterBand(1)
-    proxInBand.SetNoDataValue(noDataValue)
-    options = ['NODATA=0', 'VALUES=0']
-    gdal.ComputeProximity(srcBand, proxInBand, options)
-
-    proxIn = gdalnumeric.BandReadAsArray(proxInBand)
-    proxOut = gdalnumeric.BandReadAsArray(proxBand)
-
-    proxTotal = proxIn.astype(float) - proxOut.astype(float)
-    proxTotal = proxTotal*metersIndex
-
-    if npDistFileName != '':
-        np.save(npDistFileName, proxTotal)
-        
-    return proxTotal
-
-def createSegmentationByFeatureIndex(feature_index, rasterSrc, vectorSrc, npDistFileName='', units='pixels'):
-    dist_trans_by_feature = createDistanceTransformByFeatureIndex(feature_index, rasterSrc, vectorSrc, npDistFileName='', units='pixels')
-    dist_trans_by_feature[dist_trans_by_feature > 0] = feature_index + 1
-    dist_trans_by_feature[dist_trans_by_feature < 0] = 0  
-    return dist_trans_by_feature.astype(np.uint8)
-
-def createInstanceSegmentation(rasterSrc, vectorSrc):
-    json_data = open(vectorSrc)
-    data = json.load(json_data)
-    num_features = len(data['features'])
-    
-    cell_array = np.zeros((num_features,), dtype=np.object)
-    for i in range(num_features):
-        cell_array[i] = createSegmentationByFeatureIndex(i, rasterSrc, vectorSrc, npDistFileName='', units='pixels')
-    return cell_array
-
-def createBoundariesByFeatureIndex(feature_index, rasterSrc, vectorSrc, npDistFileName='', units='pixels'):
-    dist_trans_by_feature = createDistanceTransformByFeatureIndex(feature_index, rasterSrc, vectorSrc, npDistFileName='', units='pixels')
-    dist_trans_by_feature[dist_trans_by_feature > 1.0] = 255
-    dist_trans_by_feature[dist_trans_by_feature < -1.0] = 255
-    dist_trans_by_feature[dist_trans_by_feature != 255] = 1
-    dist_trans_by_feature[dist_trans_by_feature == 255] = 0
-    return dist_trans_by_feature.astype(np.uint8)
-
-def createInstanceBoundaries(rasterSrc, vectorSrc):
-    json_data = open(vectorSrc)
-    data = json.load(json_data)
-    num_features = len(data['features'])
-    
-    cell_array = np.zeros((num_features,), dtype=np.object)
-    for i in range(num_features):
-        full_boundary_matrix = createBoundariesByFeatureIndex(i, rasterSrc, vectorSrc, npDistFileName='', units='pixels')
-        cell_array[i] = csr_matrix(full_boundary_matrix)
-    return cell_array
-
-def createInstanceCategories(vectorSrc):
-    with open(vectorSrc) as my_file:
-        data = json.load(my_file)
-    if(len(data['features']) == 0):
-       return np.array([],dtype=np.uint8)
-    else:
-       return np.ones(len(data['features']),dtype=np.uint8).reshape((len(data['features']), 1))
 
 
 
-def geoJsonToSBD(annotationName_cls, annotationName_inst, geoJson, rasterSource):
-
-    #Print raster file name
-    my_raster_source = rasterSource
-    print("Raster directory : ",my_raster_source)
-    srcRaster = gdal.Open(my_raster_source)
-
-    
-    my_vector_source = geoJson
-    print("Vector directory : ",my_vector_source)
-
-    
-    #Call main functions to create label datafor cls
-    my_cls_segmentation = createClassSegmentation(my_raster_source, my_vector_source, npDistFileName='', units='pixels')
-    my_cls_boundaries =  createClassBoundaries(my_raster_source, my_vector_source, npDistFileName='', units='pixels')
-    my_cls_categories = createClassCategoriesPresent(my_vector_source)
-
-    #Call main functions to create label datafor inst
-    my_inst_segmentation = createInstanceSegmentation(my_raster_source, my_vector_source)
-    my_inst_boundaries = createInstanceBoundaries(my_raster_source, my_vector_source)
-    my_inst_categories = createInstanceCategories(my_vector_source)
-
-    #Wraps for cls struct
-    cls_boundaries_wrap = np.array([my_cls_boundaries])
-    cls_categories_wrap = my_cls_categories
-
-    #Wraps for inst struct
-    inst_boundaries_wrap = np.array([my_inst_boundaries])
-    inst_categories_wrap = my_inst_categories
-
-    #Create a class struct
-    GTcls = {'Segmentation': my_cls_segmentation , 'Boundaries': cls_boundaries_wrap, 'CategoriesPresent': cls_categories_wrap}
-
-
-    #Create the instance struct
-    GTinst = {'Segmentation': my_inst_segmentation , 'Boundaries': inst_boundaries_wrap, 'Categories': inst_categories_wrap}
-
-    #Save the files
-    scipy.io.savemat(annotationName_cls,{'GTcls': GTcls})
-    scipy.io.savemat(annotationName_inst,{'GTinst': GTinst})
-
-    print("Done with "+str())
-
-    entry = {'rasterFileName': my_raster_source,
-             'geoJsonFileName': geoJson,
-             'annotationName' : annotationName_cls,
-             'annotationName_cls': annotationName_cls,
-             'annotationName_inst':annotationName_inst,
-             'width': srcRaster.RasterXSize,
-             'height': srcRaster.RasterYSize,
-             'depth' : srcRaster.RasterCount,
-             'basename': os.path.splitext(os.path.basename(my_raster_source))[0]
-             }
-
-    return entry
 def convertGTiffTo8Bit(rasterImageName, outputImageName, outputFormat='GTiff',
                        minPercent=2,
                        maxPercent=98,
---
+++ python/spaceNetUtilities/labelTools/sbdLabel.py
@@ -0,0 +1,231 @@
+import numpy as np
+import os
+from spaceNetUtilities.labelTools import coreLabelTools as clT
+import math
+import pickle
+import csv
+import glob
+from PIL import Image
+from xml.etree.ElementTree import Element, SubElement, Comment, tostring
+from xml.etree import ElementTree
+from xml.dom import minidom
+import subprocess
+import scipy.io
+from scipy.sparse import csr_matrix
+import json
+import re
+import shapely
+import fiona
+import geopandas as gpd
+import rasterio
+from scipy.ndimage import morphology
+from rasterio import features
+from shapely.geometry.polygon import Polygon
+from shapely.geometry.multipolygon import MultiPolygon
+from shapely.geometry.linestring import LineString
+from shapely.geometry.multilinestring import MultiLineString
+from shapely.geometry import shape, box
+from shapely import affinity
+from osgeo import gdal, osr, ogr, gdalnumeric
+
+def createClassSegmentation(rasterSrc, vectorSrc, npDistFileName='', units='pixels'):
+    dist_trans = clT.createDistanceTransform(rasterSrc, vectorSrc, npDistFileName='', units='pixels')
+    dist_trans[dist_trans > 0] = 1
+    dist_trans[dist_trans < 0] = 0
+    return dist_trans
+
+
+def createClassBoundaries(rasterSrc, vectorSrc, npDistFileName='', units='pixels'):
+    dist_trans = clT.createDistanceTransform(rasterSrc, vectorSrc, npDistFileName='', units='pixels')
+    # From distance transform to boundary
+    dist_trans[dist_trans > 1.0] = 255
+    dist_trans[dist_trans < -1.0] = 255
+    dist_trans[dist_trans != 255] = 1
+    dist_trans[dist_trans == 255] = 0
+    sparse_total = csr_matrix(dist_trans)
+    return sparse_total.astype(np.uint8)
+
+
+def createClassCategoriesPresent(vectorSrc):
+    with open(vectorSrc) as my_file:
+        data = json.load(my_file)
+    if (len(data['features']) == 0):
+        return np.array([], dtype=np.uint8)
+    else:
+        return np.array([1], dtype=np.uint8)
+
+
+def createDistanceTransformByFeatureIndex(feature_index, rasterSrc, vectorSrc, npDistFileName='', units='pixels'):
+    ## open source vector file that truth data
+    source_ds = ogr.Open(vectorSrc)
+    source_layer = source_ds.GetLayer()
+
+    # Define feature
+    my_feature = source_layer[feature_index]
+
+    # Spatial Reference
+    srs = source_layer.GetSpatialRef()
+
+    # Create feature Layer
+    outDriver = ogr.GetDriverByName('MEMORY')
+    outDataSource = outDriver.CreateDataSource('memData')
+    Feature_Layer = outDataSource.CreateLayer("this_feature", srs, geom_type=ogr.wkbPolygon)
+
+    # Add feature to layer
+    Feature_Layer.CreateFeature(my_feature)
+
+    ## extract data from src Raster File to be emulated
+    ## open raster file that is to be emulated
+    srcRas_ds = gdal.Open(rasterSrc)
+    cols = srcRas_ds.RasterXSize
+    rows = srcRas_ds.RasterYSize
+    noDataValue = 0
+    metersIndex = 1
+
+    ## create First raster memory layer
+    memdrv = gdal.GetDriverByName('MEM')
+    dst_ds = memdrv.Create('', cols, rows, 1, gdal.GDT_Byte)
+    dst_ds.SetGeoTransform(srcRas_ds.GetGeoTransform())
+    dst_ds.SetProjection(srcRas_ds.GetProjection())
+    band = dst_ds.GetRasterBand(1)
+    band.SetNoDataValue(noDataValue)
+
+    gdal.RasterizeLayer(dst_ds, [1], Feature_Layer, burn_values=[255])
+    srcBand = dst_ds.GetRasterBand(1)
+
+    memdrv2 = gdal.GetDriverByName('MEM')
+    prox_ds = memdrv2.Create('', cols, rows, 1, gdal.GDT_Int16)
+    prox_ds.SetGeoTransform(srcRas_ds.GetGeoTransform())
+    prox_ds.SetProjection(srcRas_ds.GetProjection())
+    proxBand = prox_ds.GetRasterBand(1)
+    proxBand.SetNoDataValue(noDataValue)
+
+    options = ['NODATA=0']
+
+    gdal.ComputeProximity(srcBand, proxBand, options)
+
+    memdrv3 = gdal.GetDriverByName('MEM')
+    proxIn_ds = memdrv3.Create('', cols, rows, 1, gdal.GDT_Int16)
+    proxIn_ds.SetGeoTransform(srcRas_ds.GetGeoTransform())
+    proxIn_ds.SetProjection(srcRas_ds.GetProjection())
+    proxInBand = proxIn_ds.GetRasterBand(1)
+    proxInBand.SetNoDataValue(noDataValue)
+    options = ['NODATA=0', 'VALUES=0']
+    gdal.ComputeProximity(srcBand, proxInBand, options)
+
+    proxIn = gdalnumeric.BandReadAsArray(proxInBand)
+    proxOut = gdalnumeric.BandReadAsArray(proxBand)
+
+    proxTotal = proxIn.astype(float) - proxOut.astype(float)
+    proxTotal = proxTotal * metersIndex
+
+    if npDistFileName != '':
+        np.save(npDistFileName, proxTotal)
+
+    return proxTotal
+
+
+def createSegmentationByFeatureIndex(feature_index, rasterSrc, vectorSrc, npDistFileName='', units='pixels'):
+    dist_trans_by_feature = createDistanceTransformByFeatureIndex(feature_index, rasterSrc, vectorSrc,
+                                                                  npDistFileName='', units='pixels')
+    dist_trans_by_feature[dist_trans_by_feature > 0] = feature_index + 1
+    dist_trans_by_feature[dist_trans_by_feature < 0] = 0
+    return dist_trans_by_feature.astype(np.uint8)
+
+
+def createInstanceSegmentation(rasterSrc, vectorSrc):
+    json_data = open(vectorSrc)
+    data = json.load(json_data)
+    num_features = len(data['features'])
+
+    cell_array = np.zeros((num_features,), dtype=np.object)
+    for i in range(num_features):
+        cell_array[i] = createSegmentationByFeatureIndex(i, rasterSrc, vectorSrc, npDistFileName='', units='pixels')
+    return cell_array
+
+
+def createBoundariesByFeatureIndex(feature_index, rasterSrc, vectorSrc, npDistFileName='', units='pixels'):
+    dist_trans_by_feature = createDistanceTransformByFeatureIndex(feature_index, rasterSrc, vectorSrc,
+                                                                  npDistFileName='', units='pixels')
+    dist_trans_by_feature[dist_trans_by_feature > 1.0] = 255
+    dist_trans_by_feature[dist_trans_by_feature < -1.0] = 255
+    dist_trans_by_feature[dist_trans_by_feature != 255] = 1
+    dist_trans_by_feature[dist_trans_by_feature == 255] = 0
+    return dist_trans_by_feature.astype(np.uint8)
+
+
+def createInstanceBoundaries(rasterSrc, vectorSrc):
+    json_data = open(vectorSrc)
+    data = json.load(json_data)
+    num_features = len(data['features'])
+
+    cell_array = np.zeros((num_features,), dtype=np.object)
+    for i in range(num_features):
+        full_boundary_matrix = createBoundariesByFeatureIndex(i, rasterSrc, vectorSrc, npDistFileName='',
+                                                              units='pixels')
+        cell_array[i] = csr_matrix(full_boundary_matrix)
+    return cell_array
+
+
+def createInstanceCategories(vectorSrc):
+    with open(vectorSrc) as my_file:
+        data = json.load(my_file)
+    if (len(data['features']) == 0):
+        return np.array([], dtype=np.uint8)
+    else:
+        return np.ones(len(data['features']), dtype=np.uint8).reshape((len(data['features']), 1))
+
+
+def geoJsonToSBD(annotationName_cls, annotationName_inst, geoJson, rasterSource):
+    # Print raster file name
+    my_raster_source = rasterSource
+    print("Raster directory : ", my_raster_source)
+    srcRaster = gdal.Open(my_raster_source)
+
+    my_vector_source = geoJson
+    print("Vector directory : ", my_vector_source)
+
+    # Call main functions to create label datafor cls
+    my_cls_segmentation = createClassSegmentation(my_raster_source, my_vector_source, npDistFileName='', units='pixels')
+    my_cls_boundaries = createClassBoundaries(my_raster_source, my_vector_source, npDistFileName='', units='pixels')
+    my_cls_categories = createClassCategoriesPresent(my_vector_source)
+
+    # Call main functions to create label datafor inst
+    my_inst_segmentation = createInstanceSegmentation(my_raster_source, my_vector_source)
+    my_inst_boundaries = createInstanceBoundaries(my_raster_source, my_vector_source)
+    my_inst_categories = createInstanceCategories(my_vector_source)
+
+    # Wraps for cls struct
+    cls_boundaries_wrap = np.array([my_cls_boundaries])
+    cls_categories_wrap = my_cls_categories
+
+    # Wraps for inst struct
+    inst_boundaries_wrap = np.array([my_inst_boundaries])
+    inst_categories_wrap = my_inst_categories
+
+    # Create a class struct
+    GTcls = {'Segmentation': my_cls_segmentation, 'Boundaries': cls_boundaries_wrap,
+             'CategoriesPresent': cls_categories_wrap}
+
+    # Create the instance struct
+    GTinst = {'Segmentation': my_inst_segmentation, 'Boundaries': inst_boundaries_wrap,
+              'Categories': inst_categories_wrap}
+
+    # Save the files
+    scipy.io.savemat(annotationName_cls, {'GTcls': GTcls})
+    scipy.io.savemat(annotationName_inst, {'GTinst': GTinst})
+
+    print("Done with " + str())
+
+    entry = {'rasterFileName': my_raster_source,
+             'geoJsonFileName': geoJson,
+             'annotationName': annotationName_cls,
+             'annotationName_cls': annotationName_cls,
+             'annotationName_inst': annotationName_inst,
+             'width': srcRaster.RasterXSize,
+             'height': srcRaster.RasterYSize,
+             'depth': srcRaster.RasterCount,
+             'basename': os.path.splitext(os.path.basename(my_raster_source))[0]
+             }
+
+    return entry
