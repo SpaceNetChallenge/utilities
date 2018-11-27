9921716617ba5654416fc05b87d975221891d92a
dlindenbaum <dlindenbaum>
2018-11-27 14:52:36 -0500

    created travis.yml and tests/test_main.py for testing framework

---
+++ tests/test_main.py
@@ -0,0 +1,17 @@
+"""tests ml_export.tile_generator.base"""
+import os
+
+
+def test_import_geoTools():
+    """ Tests import of geoTools and labeltools"""
+
+    from spacenetutilities import geoTools
+
+#def test_import_evalTools():
+
+#    from spacenetutilities import evalTools
+
+#def test_import_dataTools():
+#    from spacenetutilities import dataTools
+
+#    return 0
